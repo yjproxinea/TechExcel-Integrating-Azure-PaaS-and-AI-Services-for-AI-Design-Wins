097e24590cc98015b0812f2eb5f6a0a7b46d6afe
Youssef Janjar <124138661+yjproxinea@users.noreply.github.com>
2025-02-17 13:05:02 +0000

    feat: update the cicd

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -20,6 +20,11 @@ def create_chat_completion(messages):
     aoai_endpoint = st.secrets["aoai"]["endpoint"]
     aoai_deployment_name = st.secrets["aoai"]["deployment_name"]
 
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
+
+
     client = openai.AzureOpenAI(
         azure_ad_token_provider=token_provider,
         api_version="2024-06-01",
@@ -27,13 +32,29 @@ def create_chat_completion(messages):
     )
     # Create and return a new chat completion request
     return client.chat.completions.create(
-        model=aoai_deployment_name,
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in messages
-        ],
-        stream=True
-    )
+            model=aoai_deployment_name,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in messages
+            ],
+            stream=True,
+            extra_body={
+                "data_sources": [
+                    {
+                        "type": "azure_search",
+                        "parameters": {
+                            "endpoint": search_endpoint,
+                            "index_name": search_index_name,
+                            "authentication": {
+                                "type": "api_key",
+                                "key": search_key
+                            }
+                        }
+                    }
+                ]
+            }
+        )
+
 
 def handle_chat_prompt(prompt):
     """Echo the user's prompt to the chat window.
